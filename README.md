421b17278510b6ab129a8ec6296649c0a595d1d2
Karna <karna@LAPTOP-D1JC23IV.localdomain>
2021-03-18 11:27:30 +0530

    Hadoop and lucene results in a pacge

--- README.md
+++ README.md
@@ -2,14 +2,3 @@
 
 
 Course Page : https://www.cs.ucr.edu/~vagelis/classes/CS242/index.htm
-
-
-###searchmr:
-	mvn clean compile package assembly:single
-	hadoop jar /home/karna/projects/HbaseIndex/searchmr/target/HbaseIndex-1.0.0-jar-with-dependencies.jar edu.ucr.abhi.index.Indexer <hdfs path to files>
-	
-	hadoop jar /home/karna/projects/HbaseIndex/searchmr/target/HbaseIndex-1.0.0-jar-with-dependencies.jar edu.ucr.abhi.search.Rank <query> <hdfs path to files>
-
-###spring:
-	mvn clean package spring-boot:repackage
-	java -jar target/spring-mvc-0.0.1-SNAPSHOT.jar
--- spring-mvc/src/main/java/com/unicrawl/controller/HomeController.java
+++ spring-mvc/src/main/java/com/unicrawl/controller/HomeController.java
@@ -16,23 +16,23 @@ import java.util.List;
 
 @Controller
 public class HomeController {
-    List<Output> results = new ArrayList<>();
-    // List<Output> mrResults = new ArrayList<>();
+    List<Output> lucene = new ArrayList<>();
+    List<Output> hadoop = new ArrayList<>();
     @GetMapping("/home")
     public String home(@RequestParam(required = false) String query, Model model) throws IOException, ParseException, InterruptedException {
-        // LucIndexing lin = new LucIndexing();
-        // results = lin.search(query);
-        Search search = new Search(query, "/home/karna/projects/HbaseIndex/searchmr/target/HbaseIndex-1.0.0-jar-with-dependencies.jar");
         System.out.println("Query :" + query);
-        results = search.run();
-	System.out.println("Results size :" + results.size());
+        LucIndexing lin = new LucIndexing();
+        lucene = lin.search(query);
+        Search search = new Search(query, "/home/karna/projects/HbaseIndex/searchmr/target/HbaseIndex-1.0.0-jar-with-dependencies.jar");
+        hadoop = search.run();
+	System.out.println("Hadoop results size :" + hadoop.size());
         model.addAttribute("query", query);
         return "redirect:/results";
-//        return "home_page";
     }
     @GetMapping("/results")
     public String results(Model model){
-        model.addAttribute("results", results);
+        model.addAttribute("lucene", lucene);
+        model.addAttribute("hadoop", hadoop);
         return "query_results";
     }
 }
